82b4f200dd186212530eec97a1087808d0123298
Ong <Ong@gmail.com>
2020-05-26 16:37:03 +0700

    add errTodoreducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import loadingReducer from './loading/loadingReducer';
 import setTodosReducer from './setTodosReducer';
 import todosReducer from './todosReducer';
+import errTodosReducer from './errTodosReducer'
 
 const rootReducer = combineReducers({
   isLoading : loadingReducer,
   setTodos : setTodosReducer,
-  todos : todosReducer
+  todos : todosReducer,
+  err : errTodosReducer,
 })
 
 export default rootReducer;
